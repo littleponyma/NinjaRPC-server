400f309f1b724d7c5e5b9cf5eee526ecde63abe7
Developer Ma <893947048@qq.com>
2023-04-18 18:25:52 +0800

    修改端口号，解决打包问题

---
+++ src/test/java/com/pony/ninjarpcserver/GeneralCmd.java
@@ -0,0 +1,20 @@
+package com.pony.ninjarpcserver;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+public class GeneralCmd {
+    public static void main(String[] args) {
+        JSONObject data=new JSONObject();
+        data.put("className","android.util.Log");
+        data.put("method","e");
+        JSONArray jsonArray=new JSONArray();
+        jsonArray.add("NinjaRPC");
+        jsonArray.add("test");
+        data.put("params",jsonArray);
+        JSONObject global = new JSONObject();
+        global.put("data",data);
+        System.out.println(JSON.toJSONString(global));
+    }
+}
