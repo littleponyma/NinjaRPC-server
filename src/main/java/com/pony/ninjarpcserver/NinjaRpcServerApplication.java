f780a81d3da319d813ecad8d08a913c39692093e
Developer Ma <893947048@qq.com>
2023-04-18 13:36:20 +0800

    first commit

---
+++ src/main/java/com/pony/ninjarpcserver/NinjaRpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.pony.ninjarpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaRpcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pony/ninjarpcserver/NinjaRpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pony.ninjarpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
