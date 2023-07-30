0fe1d1c4c86f24801596ee5efab5eb0e91f37a37
Janegoh Wu <893947048@qq.com>
2023-07-30 12:10:37 +0800

    移除unidbg

--- aweme_sign6_04.py
+++ aweme_sign6_04.py
@@ -1,14 +1,17 @@
 import requests
 import json
 
-url = "http://ip:端口/aweme/api/v1/sign6"
+url = "http://101.42.250.157:5888/aweme/api/v1/sign6auth"
 
 payload = json.dumps({
-  "url": "https://webcast.huoshan.com/webcast/room/enter/?luckydog_base=kL61JjwrA3bjhwtQjrwT961MjbksURDc0Uq888cLt9ncF8mB9SXVBLWXcuLKGO-uGa6vrg000crQEff5MJwC2PtmgiTh3j-NZYvMgU-89zoJhurqVmhh3gn6uBlK3XGa1nhivWaJ19Plgpik_9j3mgVj6435M6FPpM8b-86akxk&luckydog_token=vGwG3eItLthTKN36zGZGW5WUJYz3bVfs6C2AeoR9YSL4Vg18kn_A_ibtUrvmh3_gbQx1aWaASOmmwf7p5x2qCA&luckydog_data=tVrggETFyXUdT7OrpLX1rg&webcast_sdk_version=2730&webcast_language=zh&webcast_locale=zh_CN&webcast_gps_access=2&current_network_quality_info=%7B%7D&is_pad=false&is_android_pad=0&is_landscape=false&carrier_region=CN&live_sdk_version=150300&need_personal_recommend=1&iid=1269275336119278&device_id=2905346602711534&ac=wifi&channel=360_1112_0322&aid=1112&app_name=live_stream&version_code=150300&version_name=15.3.0&device_platform=android&os=android&ssmix=a&device_type=2203121C&device_brand=Xiaomi&language=zh&os_api=28&os_version=9&manifest_version_code=150300&resolution=1080*1920&dpi=280&update_version_code=15030003&_rticket=1684314321355&tab_mode=3&client_version_code=150300&mcc_mnc=46000&hs_location_permission=0&cpu_support64=true&host_abi=armeabi-v7a&rom_version=XIAOMI_unknown&cdid=4866ebb5-c3ff-4549-815f-bd9bd5956da3&new_nav=0&screen_width=617&ws_status=ConnectionState%7BState%3D1%7D&settings_version=24&last_update_time=1684314315741&cpu_model=placeholder&ts=1684314321",
-  "did": "2905346602711534",
-  "iid": "1269275336119278",
+  "url": "https://api5-normal-c-lf.amemv.com/passport/open/auth/?iid=618382549874461&device_id=3875153419465807&ac=wifi&channel=shenmasem_ls_dy_224&aid=1128&app_name=aweme&version_code=230300&version_name=23.3.0&device_platform=android&os=android&ssmix=a&device_type=Pixel+3+XL&device_brand=google&language=zh&os_api=30&os_version=11&manifest_version_code=230301&resolution=1440*2621&dpi=560&update_version_code=23309900&_rticket=1688648030477&package=com.ss.android.ugc.aweme&cpu_support64=true&host_abi=armeabi-v7a&is_guest_mode=0&app_type=normal&minor_status=0&appTheme=light&need_personal_recommend=1&is_android_pad=0&ts=1688648067&cdid=079a06d5-3907-45a3-b47f-0d2f93d847c6",
+  "did": "3875153419465807",
+  "iid": "618382549874461",
   "header": {
-    "x-ss-stub": "1908D20BF6EDE89CBB15A01468FC65D6"
+    "x-bd-client-key": "afc2a1a501b4208afe687efac6e97ead9b0055f79f55ce549e5b8763ce48223c84609486196db2930052c33d23c14e2f7ff21463cb983c77898fe100b34a3cd8",
+    "x-tt-token": "00aa9145a35bd8180496262a6dfef07c170631c76d89710de9cd6c89a60c636da34e89c028eb178d0da808e610b50a8abff69d578ce4890263b92587ed194ef974877156e219da6cbe15f5d3417cec7aa8ca729332c5999f409f32b7cc6842dd0dc72-1.0.1",
+    "x-ss-stub": "AC43A20156ACA40FF151BDF560A6561B",
+    "x-tt-dt": "AAASNYVWDMQ3D3C7ENEH2OBALHZP5L2RLLYRNJGW5VFOMN7C7OGQRE7TULCBQH674SHKZ6QOIFS5IB2VW5I2CGZJPPVQOABF436WMRPSZHTTUJ2RKEDDBMRZBKMYKDK5TL3SHD3L4XIJ7CCGOIFJREA"
   }
 })
 headers = {
